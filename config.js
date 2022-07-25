e9b8b4258ad19fd0169115b249e49c0e1fc29340
Kguom <108707076+Kguom@users.noreply.github.com>
2022-07-25 10:33:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -46,33 +46,33 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 /*============== SOCIAL ==============*/
 
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
-global.sig = 'https://instagram.com/AryBotz'
-global.sgh = 'https://github.com/arybotz'
-global.sgc = 'https://chat.whatsapp.com/JWMl9LW5g0i0RSj7ophXAk'
-global.sdc = 'https://github.com/arybotz'
-global.snh = 'https://youtube.com/c/AryBotz'
+global.sig = 'https://instagram.com/coglah_07'
+global.sgh = 'https://github.com/KhairilOFC'
+global.sgc = 'https://chat.whatsapp.com/Bn7PxZ06B3GL8BDEZhC36T'
+global.sdc = 'https://github.com/KhairilOFC'
+global.snh = 'https://youtube.com/c/Khairil🅥'
 
 /*============== PAYMENT ==============*/
-global.pdana = '085762008398'
+global.pdana = 'Gaada bang :v'
 global.povo = 'Gaada bang :v'
 global.pgopay = 'Gaada bang :v'
-global.ppulsa = '085762008398'
-global.ppulsa2 = '085762008398'
-global.psaweria = 'https://saweria.co/arybotz'
+global.ppulsa = 'Gaada bang :v'
+global.ppulsa2 = 'Gaada bang :v'
+global.psaweria = 'https://konz'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '6285763050960'
-global.nomorown = '994408465157'
-global.namebot = 'AryBotz Ofc'
-global.nameown = 'X Ary Offcial'
+global.nomorbot = '601124024797'
+global.nomorown = '60109102984'
+global.namebot = 'BebanBot天'
+global.nameown = 'Khairil春'
 
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6281264038909', 'X Ary Offcial', true]
+  ['60109102984', 'Khairil春', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
-global.mods = [] // Want some help?
+global.mods = ['60109102984'] // Want some help?
 global.prems = [] // Premium user bukan disini nambahinnya, ketik .addprem @user 10
 
 
@@ -114,12 +114,12 @@ global.APIKeys = { // APIKey Here
 // Kata APIKEY itu isi dengan apikey lu sendiri, dengan beli ke website itu
 
 /*============== WATERMARK ==============*/
-global.wm = '「 AryBotz |Ary Offcial⁩ 」' //Main Watermark
-global.wm2 = '    乂 Ary Botz 乂     '
-global.wm3 = 'Ary Botz |ᗪ'
+global.wm = '「 BebanBot天 |Khairil春 」' //Main Watermark
+global.wm2 = 'BebanBot天'
+global.wm3 = 'Khairil春 |ᗪ'
 global.botdate = `⫹⫺ 𝗗𝗮𝘁𝗲: ${week} ${date}`
 global.bottime = `𝗧 𝗜 𝗠 𝗘 : ${wktuwib}`
-global.titlebot = 'AryBotz-MD✯✯✯✯✯ '
+global.titlebot = 'BebanBot天-MD✯✯✯✯✯ '
 global.author = global.wm
 
 
@@ -173,7 +173,7 @@ global.hsquere = ['⛶','❏','⫹⫺']
 
 /*============== STICKER WM ==============*/
 global.stickpack = '\n'
-global.stickauth = `乂 YT:AryBotz.|ᗪ \nig AryBotz.id`
+global.stickauth = `乂 BebanBot天|ᗪ \nig Khairil春`
 
 global.multiplier = 38 // The higher, The harder levelup
 
