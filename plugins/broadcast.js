9aaf8701c80746609afbcaf43b789853bcd77f4b
Kguom <108707076+Kguom@users.noreply.github.com>
2022-07-25 13:41:50 +0800

    Update broadcast.js

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text }) => {
   let teks = text ? text : cc.text
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
   for (let id of chats) {
-       let bcbg = 'https://telegra.ph/file/d389f4acafac741f6592c.jpg'
+       let bcbg = 'https://telegra.ph/file/da8da7a4de36665c8ba5f.jpg'
        await conn.copyNForward(id, bcbg, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? `${htki} *BROADCAST* ${htka}\n` + teks : `${htki} *BROADCAST* ${htka}\n` + teks + '\n' + readMore + '\n\n' + botdate), true).catch(_ => _)
   m.reply('Selesai Broadcast All Chat :)')
 }
